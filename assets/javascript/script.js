920ce5ab0e1db0453be6bd418dc814b9c66e7ebe
mayamouse <cooperpontea@gmail.com>
2023-07-29 14:06:51 -0400

    added quiz questions and link to js file

---
+++ assets/javascript/script.js
@@ -0,0 +1,35 @@
+const quiz = [
+{
+    number: "1",
+    ask: "What is JavaScript?",
+    answer: ["A markup language","A server-side programming language","A client-side programming language","A database management system"],
+    correct: "A client-side programming language"
+},
+{
+    number:"2",
+    ask: "Which keyword is used to declare a variable in JavaScript?",
+    answer: ["var","let","const","all of the above"],
+    correct: "all of the above"
+},
+{
+    number: "3",
+    ask: "What is the correct way to comment a single line in JavaScript?",
+    answer: ["<!-- This is a comment -->","/* This is a comment */","// This is a comment","'This is a comment'"],
+    correct: "// This is a comment"
+},
+{
+    number: "4",
+    ask: "What is the purpose of the 'forEach' method in JavaScript",
+    answer: ["To create a new array based on the elements of the original array.",
+        "To add a new element to the beginning of an array.",
+        "To iterate over each element of an array and execute a callback function.",
+        "To sort the elements of an array in ascending order."],
+    correct: "To iterate over each element of an array and execute a callback function."
+},
+{
+    number: "5",
+    ask: "Which built-in method can be used to convert a string to all uppercase letters?",
+    answer: ["toUpperCase()","toUpper()","convertUpperCase()","uppercase()"],
+    correct: "toUpperCase()"
+}
+]
