2f3aabd25b5c423abdf65a7b22cac80b467c7c1b
mayamouse <cooperpontea@gmail.com>
2023-07-29 15:45:20 -0400

    added local storage logic to js and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # coding-timed-quizlet
+## Overview
+Created a simple quizlet that will cycle through questions about JavaScript. Quiz taker is penalized for incorrect answers (reduction in points).
+
+## Acceptance Criteria 
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+## Solutions Implemented
+
+- Quiz will cycle through 5 JavaScript Questions.
+- Quiz uses a timer.
+- Quiz adds 20 points for correct answers, and reduces by 10 points for wrong answers.
+- Quiz allows player to save their score and initials at the end of the quiz.
+
+## Sources
+https://simplestepscode.com/javascript-quiz-tutorial/
+
+## Deployed Preview
+
+## Author
+View my other projects here! 
+https://mayamous3.github.io/amanda-wow-portfolio/
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+// Define the set of quiz questions and answers in an array.
 const quiz = [
 {
     number: "1",
@@ -32,4 +33,102 @@ const quiz = [
     answer: ["toUpperCase()","toUpper()","convertUpperCase()","uppercase()"],
     correct: "toUpperCase()"
 }
-]
+];
+
+const startButton = document.getElementById('startButton');
+const endButton = document.getElementById('endButton');
+const quizContainer = document.getElementById('quizzy');
+const timerElement = document.getElementById('timer');
+const scoreElement = document.getElementById('pointTotal');
+
+let currentQuestionIndex = 0;
+let score = 0;
+let timeLeft = 60; // Total time for the quiz in seconds
+let timerInterval;
+
+// Function to display the current question
+function displayQuestion() {
+    const questionObj = quiz[currentQuestionIndex];
+    const questionElement = document.createElement('div');
+    questionElement.innerHTML = `
+        <p>${questionObj.number}. ${questionObj.ask}</p>
+    `;
+
+    const answerList = document.createElement('ul');
+    questionObj.answer.forEach((answer) => {
+        const answerItem = document.createElement('li');
+        answerItem.textContent = answer;
+        answerList.appendChild(answerItem);
+    });
+
+    answerList.addEventListener('click', handleAnswerClick);
+    questionElement.appendChild(answerList);
+    quizContainer.innerHTML = '';
+    quizContainer.appendChild(questionElement);
+}
+
+// Function for the click event on answers
+function handleAnswerClick(event) {
+    const selectedAnswer = event.target.textContent;
+    const questionObj = quiz[currentQuestionIndex];
+    if (selectedAnswer === questionObj.correct) {
+        score += 20;
+    } else {
+        score -= 10;
+    }
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex < quiz.length) {
+        displayQuestion();
+    } else {
+        endQuiz();
+    }
+}
+
+// Function to start the quiz
+function startQuiz() {
+    startButton.style.display = 'none';
+    endButton.style.display = 'inline';
+    displayQuestion();
+    timerInterval = setInterval(updateTimer, 1000);
+}
+
+// Function to end the quiz
+function endQuiz() {
+    clearInterval(timerInterval);
+    quizContainer.innerHTML = '<p>Quiz Completed!</p>';
+    scoreElement.textContent = `Final Score: ${score}`;
+
+    // Save the score to local storage
+    localStorage.setItem('userScore', score);
+}
+
+// Function to update the timer
+function updateTimer() {
+    timeLeft--;
+    timerElement.textContent = `Time Left: ${timeLeft} seconds`;
+
+    if (timeLeft <= 0) {
+        endQuiz();
+    }
+}
+
+// Event listener for the start button
+startButton.addEventListener('click', startQuiz);
+
+// Event listener for the end button
+endButton.addEventListener('click', endQuiz);
+
+// Use local Storage to populate the final userScore
+window.addEventListener('load', function() {
+    const savedScore = localStorage.getItem('userScore');
+    if (savedScore !== null) {
+        score = parseInt(savedScore);
+        scoreElement.textContent = `Final Score: ${score}`;
+    }
+});
+
+// Clear local storage using event listener 'before unload' so that score doesn't persist when page is refreshed.
+window.addEventListener('beforeunload', function() {
+    localStorage.removeItem('userScore');
+});
